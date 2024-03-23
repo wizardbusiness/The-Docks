c2082fa3ca30a43b89cb91443b1cb2d217cb695e
Gabriel <gkime91@gmail.com>
2024-03-23 11:58:53 -0700

    created simple node express app with dockerfile

---
+++ SimpleNodeApp/src/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("This is a node app running in a docker container!");
+});
+
+app.listen(3000, () => {
+  console.log("app listening on port 3000")
+})
