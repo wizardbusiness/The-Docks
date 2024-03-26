d66233e8866047bda4a319d60f5da05a17c54c10
Gabriel <gkime91@gmail.com>
2024-03-25 22:54:21 -0700

    form now updates profile on submit

--- SimpleAppWithDB/public/index.js
+++ SimpleAppWithDB/public/index.js
@@ -5,7 +5,6 @@ function handleShowForm() {
   let showForm = false;
   return () => {
     showForm === true ? showForm = false : showForm = true;
-    console.log(showForm)
     if (showForm) {
       form.style.display = 'block';
       info.style.display = 'none';
@@ -16,9 +15,33 @@ function handleShowForm() {
   }
 }
 
-const toggleShowForm = handleShowForm();
+function handleUpdateFormFields() {
 
-const editBtn = document.querySelector("#edit-btn")
-editBtn.addEventListener("click", toggleShowForm)
+}
+
+const toggleShowForm = handleShowForm();
+const editBtn = document.querySelector("#edit-btn");
+editBtn.addEventListener("click", toggleShowForm);
+
+
+const submitEditBtn = document.querySelector("#submit-edit-btn")
+function submitEdit(e) {
+  e.preventDefault();
+  const inputFields= document.querySelectorAll("input");
+  const displayElements = document.querySelectorAll("h3");
+  inputFields.forEach((field) => {
+    displayElements.forEach(element => {
+      if (field.id === element.id) {
+        element.textContent = field.value;
+      }
+    })
+  })
+  toggleShowForm();
+}
+
+const form = document.querySelector("form");
+form.addEventListener("submit", submitEdit)
+
+
 
 
