67b8cc8c539b888ecacb7cdece68f65adf357d18
Gabriel <gkime91@gmail.com>
2024-03-26 22:51:36 -0700

    added get and post routes and controllers to toy server

--- SimpleAppWithDB/src/server.js
+++ SimpleAppWithDB/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
+const MongoClient = require('mongodb').MongoClient;
+
 
 const app = express();
 
@@ -11,6 +12,38 @@ app.get('/', (req, res) => {
 })
 
 app.use(express.static('../public'));
+
+app.get('/get-profile', (req, res) => {
+  const response = res;
+
+  MongoClient.connect('mongodb://admin:pass@localhost:27017', (err, client) => {
+    if (err) throw err;
+    const db = client.db('catbook-db');
+    const query = {catid: 1};
+    db.collection('cats').findOne(query, (err, result) => {
+      if (err) throw err;
+      client.close();
+      response.send(result)
+    });
+  });
+});
+
+app.post('/update-profile', (req, res) => {
+  const userObj = req.body;
+  console.log('connecting to db...')
+  MongoClient.connect('mongodb://admin:pass@localhost:27017', () => {
+    if (err) throw err;
+    let db = client.db('catbook-db')
+    userObj['userid'] = 1;
+    const query = {userId: 1};
+    const newValues = { $set: userObj};
+  
+    db.collection("cats").updateOne(query, newValues, {upsert: true}, (err, res) => {
+      if (err) throw err;
+      client.close()
+    });
+  });
+})
 
 app.listen(PORT, () => {
   console.log(`Cat napping on port ${PORT}`)
